b7f98e2bd9fda420ce6ad27f97d682fa024ca041
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2014-01-13 23:51:23 +0100

    improving scaffolding

---
+++ bin/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+mkdir -vp ./app/ ./static/ ./bin/ ./log/ ./tmp/
+
+mkdir -vp ./app/assets/ ./app/routes/ ./app/tests/ ./app/views/ ./app/config/ ./app/models/ ./app/utils/ ./app/lang/
+
+mkdir -vp ./app/assets/stylus/ ./app/assets/images/
+
+mkdir -vp ./static/css/ ./static/js/ ./static/ico/ ./static/img/ ./static/js/ ./static/font/
